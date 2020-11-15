1a1879453d6a556067402ca3f670a8abc70812e5
NirobNabil <thetwin11100@gmail.com>
2020-11-16 05:17:23 +0600

    gg

--- server/index.js
+++ server/index.js
@@ -9,21 +9,22 @@ app.use(cors());
 app.get('/', (req, res, next) => res.send('Hello world!'));
 app.get('/port', (req, res, next) => {
   res.set('Content-Type', 'Application/json');  
-  res.send(JSON.stringify({'port': process.env.PORT || 9000}))
+  res.send(JSON.stringify({'port': process.env.PORT || 7000}))
 });
 app.get('/receive', (req,res,next) => res.sendFile(path.join(__dirname+'/../client/receiver.html')));
 app.get('/send', (req,res,next) => res.sendFile(path.join(__dirname+'/../client/sender.html')));
-app.get('/debug', (req,res,next) => res.send(JSON.stringify(clientOO)))
-const server = app.listen(process.env.PORT || 9000);
+app.get('/debug', (req,res,next) => res.send(JSON.stringify()))
+const server = app.listen(process.env.PORT || 7000);
 
 const peerServer = ExpressPeerServer(server, {
   path: '/',
   allow_discovery: true,
-  expire_timeout: 50000,
+  expire_timeout: 20000,
   debug:1
 });
 
 var clientOO;
-peerServer.on('connection', (client) => { clientOO=client; console.log(client) });
+peerServer.on('connection', (client) => { clientOO=client; console.log("conn - " + client.id) });
+peerServer.on('disconnect', (client) => { clientOO=client; console.log("disconn - " + client.id) });
 
 app.use('/peerjs', peerServer);
