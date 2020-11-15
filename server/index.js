c366d3d2bb48074ef7df072705b0c2cb5b706e08
NirobNabil <thetwin11100@gmail.com>
2020-11-16 03:29:53 +0600

    lost track again :)

--- server/index.js
+++ server/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const { ExpressPeerServer } = require('peer');
-
+const path = require('path');
 const app = express();
 
 app.get('/', (req, res, next) => res.send('Hello world!'));
-
+app.get('/port', (req, res, next) => {
+  res.set('Content-Type', 'Application/json');  
+  res.send(JSON.stringify({'port': process.env.PORT || 9000}))
+});
+app.get('/receive', (req,res,next) => res.sendFile(path.join(__dirname+'/../client/receiver.html')));
+app.get('/send', (req,res,next) => res.sendFile(path.join(__dirname+'/../client/sender.html')));
 const server = app.listen(process.env.PORT || 9000);
 
 const peerServer = ExpressPeerServer(server, {
-  path: '/myapp'
+  path: '/',
+  allow_discovery: true,
+  expire_timeout: 500000
 });
+
+peerServer.on('connection', (client) => { console.log(client) });
 
 app.use('/peerjs', peerServer);
