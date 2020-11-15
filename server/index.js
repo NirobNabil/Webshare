561979fee5e193846d4e33e7e9f80fded9d1de36
NirobNabil <thetwin11100@gmail.com>
2020-11-16 00:59:47 +0600

    testing peerjs broker server example

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { ExpressPeerServer } = require('peer');
+
+const app = express();
+
+app.get('/', (req, res, next) => res.send('Hello world!'));
+
+const server = app.listen(9000);
+
+const peerServer = ExpressPeerServer(server, {
+  path: '/myapp'
+});
+
+app.use('/peerjs', peerServer);
